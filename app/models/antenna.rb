6746242a87f4c1e11e44177e194d59d697cdffd0
Imtiaz A. Nizami <imtiazan@gmail.com>
2012-10-19 13:11:27 +0000

    Antenna validation bugs fixed

--- app/models/antenna.rb
+++ app/models/antenna.rb
@@ -3,12 +3,11 @@ class Antenna < ActiveRecord::Base
   # Validations
   #validate :count_within_limit
   validate :correct_band_populated
-  validate :correct_antenna_band
-  validate :correct_antenna_tilts
+  validate :correct_antenna_band_and_tilts
   validates_presence_of :sector_id
   validates_numericality_of :azimuth, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 360, :allow_nil => true
   validates_numericality_of :hba, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100, :allow_nil => true
-  validates_numericality_of :mechanical_tilt, :greater_than_or_equal_to => -14, :less_than_or_equal_to => 14, :allow_nil => true
+  validates_numericality_of :mechanical_tilt, :greater_than_or_equal_to => -14, :less_than_or_equal_to => 100, :allow_nil => true
 
 
   # Relations
@@ -25,27 +24,63 @@ class Antenna < ActiveRecord::Base
 
   def correct_band_populated
     unless ( ( band == "900" && (electrical_tilt_1800.blank? && electrical_tilt_2100.blank?) ) ||
-             ( band == "1800" && (electrical_tilt_900.blank? && electrical_tilt_2100.blank?) ) )
+             ( band == "1800" && (electrical_tilt_900.blank? && electrical_tilt_2100.blank?) ) ||
+             ( band == "Dual" && (electrical_tilt_2100.blank?) ) )
       errors.add(:base, "Error: Electrical tilt entered is for the wrong band.")
     end
   end
 
-  def correct_antenna_band
-    unless (@@antenna_specs[code].band == band)
-      errors.add(:base, "Error: Selected antenna does not support selected band.")
-    end
-  end
+  Specs = Struct.new(:band, :electrical_tilt_900_upper, :electrical_tilt_1800_upper)
+  def correct_antenna_band_and_tilts
+    antenna_specs = {
+      "Andrew: DB874G35A-XY" => Specs.new("1800", nil, nil),
+      "Andrew: HBX-6516DS-VTM" => Specs.new("1800", nil, 10.0),
+      "Andrew: HBX-6517DS-VTM" => Specs.new("1800", nil, 6.0),
+      "Andrew: HBX-9014DS-VTM" => Specs.new("1800", nil, 10.0),
+      "Andrew: DBXLH-6565A-VTM" => Specs.new("Dual", 15.0, 8.0),
+      "Andrew: DBXLH-6565B-VTM" => Specs.new("Dual", 10.0, 6.0),
+      "Andrew: DBXLH-6565C-VTM" => Specs.new("Dual", 8.0, 6.0),
+      "Andrew: LBX-6513DS-VTM" => Specs.new("900", 15.0, nil),
+      "Andrew: LBX-6516DS-VTM" => Specs.new("900", 8.0, nil),
+      "Andrew: LBX-9013DS-VTM" => Specs.new("900", 10.0, nil),
+      "Andrew: LBXX-6516DS-VTM" => Specs.new("900", 8.0, nil),
+      "Kathrein: 80010137" => Specs.new("Dual", nil, nil),
+      "Kathrein: 741989" => Specs.new("1800", nil, 8.0),
+      "Kathrein: 742213" => Specs.new("1800", nil, 6.0),
+      "Kathrein: 742215" => Specs.new("1800", nil, 10.0),
+      "Kathrein: 742265" => Specs.new("Dual", 10.0, 6.0),
+      "Kathrein: 742266" => Specs.new("Dual", 7.0, 6.0),
+      "Kathrein: 80010248" => Specs.new("Dual", nil, nil),
+      "Kathrein: 739665" => Specs.new("900", 10.0, nil),
+      "Kathrein: 739684" => Specs.new("900", 14.0, nil),
+      "Kathrein: 739686" => Specs.new("900", 7.0, nil),
+      "Mobi: MB6F-03 5-c-i-s" => Specs.new("Dual", nil, nil),
+      "Mobi: MB3F-65-18DE10" => Specs.new("1800", nil, 10.0),
+      "Mobi: MB3F-65-20DE" => Specs.new("1800", nil, 10.0),
+      "Mobi: MB3F-90-17DE" => Specs.new("1800", nil, 10.0),
+      "Mobi: MB3BH/3F-65-16/17DE" => Specs.new("Dual", 10.0, 8.0),
+      "Mobi: MB3BH/3F-65-17/18DE" => Specs.new("Dual", 10.0, 8.0),
+      "Mobi: MB3BH-3F-65-14/17DE" => Specs.new("Dual", 14.0, 10.0),
+      "Mobi: MB7f-d7/9-wc-001" => Specs.new("Dual", 0.0, 0.0),
+      "Mobi: MB3BH-65-15DE" => Specs.new("900", 14.0, nil),
+      "Mobi: MB3BH-65-18-DDE" => Specs.new("900", 12.0, nil),
+      "Mobi: MB3BH-65-18-DE" => Specs.new("900", 12.0, nil),
+      "Mobi: MB3BH-90-16.5DE10" => Specs.new("900", 10.0, nil)
+    }
 
-  def correct_antenna_tilts
+    unless (antenna_specs[code].band == band)
+      errors.add(:base, "Error: Selected antenna supports bands: #{antenna_specs[code].band}")
+    end
+
     unless (electrical_tilt_900.nil?)
-      if ( @@antenna_specs[code].electrical_tilt_900_upper.nil? or electrical_tilt_900 < 0 or electrical_tilt_900 > @@antenna_specs[code].electrical_tilt_900_upper )
-        errors.add(:base, "Error: Selected antenna does not support selected 900 tilt.")
+      if ( antenna_specs[code].electrical_tilt_900_upper.nil? or electrical_tilt_900 < 0 or electrical_tilt_900 > antenna_specs[code].electrical_tilt_900_upper )
+        errors.add(:base, "Error: Selected antenna's 900 tilt range is: 0-#{antenna_specs[code].electrical_tilt_900_upper||0.to_s}")
       end
     end
 
     unless (electrical_tilt_1800.nil?)
-      if ( @@antenna_specs[code].electrical_tilt_1800_upper.nil? or electrical_tilt_1800 < 0 or electrical_tilt_1800 > @@antenna_specs[code].electrical_tilt_1800_upper )
-        errors.add(:base, "Error: Selected antenna does not support selected 1800 tilt.")
+      if ( antenna_specs[code].electrical_tilt_1800_upper.nil? or electrical_tilt_1800 < 0 or electrical_tilt_1800 > antenna_specs[code].electrical_tilt_1800_upper )
+        errors.add(:base, "Error: Selected antenna's 1800 tilt range is: 0-#{antenna_specs[code].electrical_tilt_1800_upper||0.to_s}")
       end
     end
   end
@@ -75,41 +110,5 @@ class Antenna < ActiveRecord::Base
 
 
   #
-  Specs = Struct.new(:band, :electrical_tilt_900_upper, :electrical_tilt_1800_upper)
-  @@antenna_specs = {
-    "Andrew: DB874G35A-XY" => Specs.new("1800", nil, nil),
-    "Andrew: HBX-6516DS-VTM" => Specs.new("1800", nil, 10.0),
-    "Andrew: HBX-6517DS-VTM" => Specs.new("1800", nil, 6.0),
-    "Andrew: HBX-9014DS-VTM" => Specs.new("1800", nil, 10.0),
-    "Andrew: DBXLH-6565A-VTM" => Specs.new("dual", 15.0, 8.0),
-    "Andrew: DBXLH-6565B-VTM" => Specs.new("dual", 10.0, 6.0),
-    "Andrew: DBXLH-6565C-VTM" => Specs.new("dual", 8.0, 6.0),
-    "Andrew: LBX-6513DS-VTM" => Specs.new("900", 15.0, nil),
-    "Andrew: LBX-6516DS-VTM" => Specs.new("900", 8.0, nil),
-    "Andrew: LBX-9013DS-VTM" => Specs.new("900", 10.0, nil),
-    "Andrew: LBXX-6516DS-VTM" => Specs.new("900", 8.0, nil),
-    "Kathrein: 80010137" => Specs.new("dual", nil, nil),
-    "Kathrein: 741989" => Specs.new("1800", nil, 8.0),
-    "Kathrein: 742213" => Specs.new("1800", nil, 6.0),
-    "Kathrein: 742215" => Specs.new("1800", nil, 10.0),
-    "Kathrein: 742265" => Specs.new("dual", 10.0, 6.0),
-    "Kathrein: 742266" => Specs.new("dual", 7.0, 6.0),
-    "Kathrein: 80010248" => Specs.new("dual", nil, nil),
-    "Kathrein: 739665" => Specs.new("900", 10.0, nil),
-    "Kathrein: 739684" => Specs.new("900", 14.0, nil),
-    "Kathrein: 739686" => Specs.new("900", 7.0, nil),
-    "Mobi: MB6F-03 5-c-i-s" => Specs.new("dual", nil, nil),
-    "Mobi: MB3F-65-18DE10" => Specs.new("1800", nil, 10.0),
-    "Mobi: MB3F-65-20DE" => Specs.new("1800", nil, 10.0),
-    "Mobi: MB3F-90-17DE" => Specs.new("1800", nil, 10.0),
-    "Mobi: MB3BH/3F-65-16/17DE" => Specs.new("dual", 10.0, 8.0),
-    "Mobi: MB3BH/3F-65-17/18DE" => Specs.new("dual", 10.0, 8.0),
-    "Mobi: MB3BH-3F-65-14/17DE" => Specs.new("dual", 14.0, 10.0),
-    "Mobi: MB7f-d7/9-wc-001" => Specs.new("dual", 0.0, 0.0),
-    "Mobi: MB3BH-65-15DE" => Specs.new("900", 14.0, nil),
-    "Mobi: MB3BH-65-18-DDE" => Specs.new("900", 12.0, nil),
-    "Mobi: MB3BH-65-18-DE" => Specs.new("900", 12.0, nil),
-    "Mobi: MB3BH-90-16.5DE10" => Specs.new("900", 10.0, nil)
-  }
 
 end
